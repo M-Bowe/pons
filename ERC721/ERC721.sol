fa0f01d797dfd6f5d84715d1a5944d5b87878f7c
root <root@pons.se.rit.edu>
2018-10-31 21:44:54 -0400

    Added ERC721 Wrapper

--- ERC721/ERC721.sol
+++ ERC721/ERC721.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.24;
 import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol';
 import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Mintable.sol';
 
-contract MyNFT is ERC721Full, ERC721Mintable {
-  constructor() ERC721Full("MyNFT", "MNFT") public {
+contract PonsERC721 is ERC721Full, ERC721Mintable {
+  constructor() ERC721Full("PERC", "PERC") public {
   }
 }
