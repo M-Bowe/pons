30ca4e7b59beb802a6e2747d861cb17ad519dea8
root <root@pons.se.rit.edu>
2018-10-31 21:40:35 -0400

    added ERC721 Compiler

--- ERC721/build-wrapper.sh
+++ ERC721/build-wrapper.sh
@@ -1,5 +1,5 @@
 rm -R generated
 mkdir generated
-solc Remix/ERC721Token.sol --bin --abi --optimize -o generated/ --overwrite
+solc ERC721.sol --bin --abi --optimize -o generated/ --overwrite
 #web3j solidity generate generated/MonetaryToken.bin generated/MonetaryToken.abi -o ../Bridge/src/main/java -p com.pons.Contracts
 echo "Created Wrapper Class"
