9d626bc6ade4a098fe96426e1eda5de8ddf768ca
Nathan Connor <nxc5929@rit.edu>
2018-11-04 17:06:35 -0500

    Added ERC20 to correct location

--- ERC20/build-wrapper.sh
+++ ERC20/build-wrapper.sh
@@ -1,5 +1,5 @@
 rm -R generated
 mkdir generated
 solc ERC20-Token.sol --bin --abi --optimize -o generated/ --overwrite
-web3j solidity generate generated/MonetaryToken.bin generated/MonetaryToken.abi -o ../Bridge/src/main/java -p com.pons.contracts
+web3j solidity generate generated/MonetaryToken.bin generated/MonetaryToken.abi -o ../Bridge/src/main/java -p com.pons.bridge.contracts
 echo "Created Wrapper Class"
