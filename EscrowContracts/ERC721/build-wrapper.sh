4770bba080c60b8b47fe508ce53422dabec18077
Colin Fausnaught <cjf1613@rit.edu>
2019-01-18 10:41:42 -0500

    Added ERC721 Escrow Contract

---
+++ EscrowContracts/ERC721/build-wrapper.sh
@@ -0,0 +1,5 @@
+rm -R generated
+mkdir generated
+solc EscrowERC721.sol --bin --abi --optimize -o generated/ --overwrite
+web3j solidity generate generated/EscrowERC721.bin generated/EscrowERC721.abi -o ../Bridge/src/main/java -p com.pons.bridge.contracts
+echo "Created Wrapper Class"
