b59975e25701d19ca579cb42785a194fbcb728a6
Colin Fausnaught <cjf1613@rit.edu>
2019-03-15 15:19:12 -0400

    gas change

--- Bridge/src/main/java/com/pons/bridge/web3j/DeployGasProvider.java
+++ Bridge/src/main/java/com/pons/bridge/web3j/DeployGasProvider.java
@@ -4,14 +4,15 @@ import java.math.BigInteger;
 
 import org.web3j.tx.Contract;
 import org.web3j.tx.gas.StaticGasProvider;
+import org.web3j.tx.gas.DefaultGasProvider;
 
 public class DeployGasProvider extends StaticGasProvider {
-	
-	public static final BigInteger GAS_LIMIT = Contract.GAS_LIMIT;
-    public static final BigInteger GAS_PRICE = new BigInteger("0");
+
+	public static final BigInteger GAS_LIMIT = DefaultGasProvider.GAS_LIMIT;
+	public static final BigInteger GAS_PRICE = DefaultGasProvider.GAS_PRICE;
 
 	public DeployGasProvider() {
 		super(GAS_PRICE, GAS_LIMIT);
 	}
-	
+
 }
