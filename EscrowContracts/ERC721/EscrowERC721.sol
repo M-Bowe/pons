344149066f7583bea62d29ead123056ce199434d
Nathan Connor <nxc5929@rit.edu>
2019-03-21 19:16:13 -0400

    removed interface fom EscrowERC721 because no bin was being generated (it was an interface)

--- EscrowContracts/ERC721/EscrowERC721.sol
+++ EscrowContracts/ERC721/EscrowERC721.sol
@@ -1,8 +1,6 @@
 pragma solidity ^0.4.25;
 
-import "./IERC721Receiver.sol";
-
-contract EscrowERC721 is IERC721Receiver{
+contract EscrowERC721{
 
     address private owner;
     address private fromERC721;
