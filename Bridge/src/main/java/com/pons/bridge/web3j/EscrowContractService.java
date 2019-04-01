7a58da4b16d0cea6b0a3214d0cb1e3aee601b412
Colin Fausnaught <cjf1613@rit.edu>
2019-04-01 12:14:42 -0400

    added debug info

--- Bridge/src/main/java/com/pons/bridge/web3j/EscrowContractService.java
+++ Bridge/src/main/java/com/pons/bridge/web3j/EscrowContractService.java
@@ -22,7 +22,7 @@ public class EscrowContractService {
 	private Credentials credentials;
 	private MasterNode masterNode;
 	private ContractGasProvider gasProvider;
-	
+
 	public EscrowContractService(){
 		masterNode = MasterNode.getInstance();
 		erc20Web3j = masterNode.getWeb3j();
@@ -30,11 +30,11 @@ public class EscrowContractService {
 		credentials = masterNode.getCredentials();
 		gasProvider = new DeployGasProvider();
 	}
-	
+
 	/***************************************************************************************************************
 	 ************************************************ ERC 20 *******************************************************
 	 **************************************************************************************************************/
-	
+
 	public String createEscrowOnERC20(String from, String to, BigInteger quantity, String passcode){
 		System.out.println("Deploying Escrow to ERC20 Chain");
 		EscrowERC20 escrowContract;
@@ -49,14 +49,15 @@ public class EscrowContractService {
         System.out.println("Escrow deployed to ERC20 Chain: " + contractAddress);
         return contractAddress;
 	}
-	
+
 	public EscrowERC20 loadEscrowfromERC20Chain(String contractAddress){
 		ContractGasProvider contractGasProvider = new DeployGasProvider();
 		return EscrowERC20.load(contractAddress, erc20Web3j, credentials, contractGasProvider);
 	}
-	
+
 	public boolean approveERC20(String address, String passcode) {
 		EscrowERC20 erc20 = loadEscrowfromERC20Chain(address);
+		System.out.println("Address: " + address + "  Passcode: " + passcode + "  erc721Address: " + masterNode.getErc20TokenAddress());
 		try {
 			return erc20.freeFromEscrow(masterNode.getErc20TokenAddress(), passcode).send().isStatusOK();
 		} catch (Exception e) {
@@ -64,18 +65,18 @@ public class EscrowContractService {
 		}
 		return false;
 	}
-	
+
 	/***************************************************************************************************************
 	 ************************************************ ERC 721 ******************************************************
 	 **************************************************************************************************************/
-	
+
 	public String createEscrowOnERC721(String from, String to, BigInteger tokenId, String passcode){
         System.out.println("Deploying Escrow to ERC721 Chain");
         EscrowERC721 escrowContract;
 		try {
 			escrowContract = EscrowERC721.deploy(erc721Web3j, credentials, gasProvider).send();
 			escrowContract.setParams(from, to, passcode, tokenId).send();
-			System.out.println("From: " + from + "  To: " + to + "  Passcode: " + passcode + "  TokenID: " + tokenId);		
+			System.out.println("From: " + from + "  To: " + to + "  Passcode: " + passcode + "  TokenID: " + tokenId);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
@@ -84,7 +85,7 @@ public class EscrowContractService {
         System.out.println("Escrow deployed to ERC721 Chain: " + contractAddress);
         return contractAddress;
 	}
-	
+
 	public EscrowERC721 loadEscrowfromERC721Chain(String contractAddress){
 		ContractGasProvider contractGasProvider = new DeployGasProvider();
 		return EscrowERC721.load(contractAddress, erc721Web3j, credentials, contractGasProvider);
@@ -92,7 +93,7 @@ public class EscrowContractService {
 
 	public boolean approveERC721(String address, String passcode) {
 		EscrowERC721 erc721 = loadEscrowfromERC721Chain(address);
-		System.out.println("Addeess: " + address + "  Passcode: " + passcode + "  erc721Address: " + masterNode.getErc721ContractAddress());
+		System.out.println("Address: " + address + "  Passcode: " + passcode + "  erc721Address: " + masterNode.getErc721ContractAddress());
 		try {
 			return erc721.freeFromEscrow(masterNode.getErc721ContractAddress(), passcode).send().isStatusOK();
 		} catch (Exception e) {
@@ -100,5 +101,5 @@ public class EscrowContractService {
 		}
 		return false;
 	}
-   
+
 }
